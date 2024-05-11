cf961e17d08f75fe0977eb50c3b9ad486c1607cc
Sumukha <74605929+winterscar922@users.noreply.github.com>
2024-05-11 14:23:31 +0530

    Implemented two get API's

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        private readonly ILogger _logger;
+
+        public ProductsController(StoreContext context, ILogger logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> getAllProducts()
+        {
+            try
+            {
+                var response = await _context.Products.ToListAsync();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in getAllProducts API");
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> getProductById(int id)
+        {
+            try
+            {
+                var response = await _context.Products.FindAsync(id);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in getProductById API");
+                throw;
+            }
+        }
+    }
+}
